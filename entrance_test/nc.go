f9353a1c64398269de7601f9c32b315e395f96f4
abogovski <abogovski@gmail.com>
2017-08-14 20:35:56 +0300

    gofmt entrance_test

--- entrance_test/nc.go
+++ entrance_test/nc.go
@@ -1,57 +1,57 @@
 package main
 
 import (
-    "fmt"
-    "os"
-    "io"
-    "flag"
-    "strings"
+	"flag"
+	"fmt"
+	"io"
+	"os"
+	"strings"
 )
 
-func Check(err error) {
-    if err != nil {	
-	fmt.Fprintf(os.Stderr, err.Error())
-        os.Exit(1)
-    }
-}
-
-func CountNewlines(filename string, buf_size uint) uint64 {
-    var newline_cnt uint64 = 0
-    buf := make([]byte, buf_size)
-
-    f, err := os.Open(filename)
-    Check(err)
-
-    for {
-        n_read, err := f.Read(buf)
-        newline_cnt = newline_cnt + uint64(strings.Count(string(buf[:n_read]), "\n"))
+func check(err error) {
 	if err != nil {
-            f.Close()
-	    if err == io.EOF {
-	        return newline_cnt
-	    }
-	    Check(err)
+		fmt.Fprintf(os.Stderr, err.Error())
+		os.Exit(1)
 	}
-    }
 }
 
-func Usage() {
-    fmt.Fprintf(os.Stderr, "\n")
-    fmt.Fprintf(os.Stderr, "Usage:\n  %s [flags] filename\n", os.Args[0])
-    fmt.Fprintf(os.Stderr, "\n")
-    fmt.Fprintf(os.Stderr, "Flags:\n")
-    flag.PrintDefaults()
+func countNewlines(filename string, bufSize uint) uint64 {
+	var newlineCnt uint64
+	buf := make([]byte, bufSize)
+
+	f, err := os.Open(filename)
+	check(err)
+
+	for {
+		nRead, err := f.Read(buf)
+		newlineCnt = newlineCnt + uint64(strings.Count(string(buf[:nRead]), "\n"))
+		if err != nil {
+			f.Close()
+			if err == io.EOF {
+				return newlineCnt
+			}
+			check(err)
+		}
+	}
+}
+
+func usage() {
+	fmt.Fprintf(os.Stderr, "\n")
+	fmt.Fprintf(os.Stderr, "Usage:\n  %s [flags] filename\n", os.Args[0])
+	fmt.Fprintf(os.Stderr, "\n")
+	fmt.Fprintf(os.Stderr, "Flags:\n")
+	flag.PrintDefaults()
 }
 
 func main() {
-    flag.Usage = Usage
-    buf_size := flag.Uint("b", 4096 * 16, "buffer size") // default value ~ 16 disc sectors
-    flag.Parse()
+	flag.Usage = usage
+	bufSize := flag.Uint("b", 4096*16, "Buffer Size") // default value ~ 16 disc sectors
+	flag.Parse()
 
-    if len(flag.Args()) != 1 {
-        Usage()
-	os.Exit(1)
-    } else {
-        fmt.Println(CountNewlines(flag.Arg(0), *buf_size))
-    }
+	if len(flag.Args()) != 1 {
+		usage()
+		os.Exit(1)
+	} else {
+		fmt.Println(countNewlines(flag.Arg(0), *bufSize))
+	}
 }
